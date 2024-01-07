2aa1bd7a44014a7387037f83a8ff9d6ed5c4f10f
MaribelC <Maribel_Calvario@yahoo.com>
2024-01-07 02:00:49 -0600

    created index.js

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// Require thoughts Models.
+const thoughts = require('./thoughts');
+// Require users Models.
+const users = require('./users');
+
+
+//Export Modules.
+module.exports = { users, thoughts};
